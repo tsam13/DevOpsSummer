bdc26b8f1f5980d54ac3220f20a0c3e99ea1ff16
trist <trist@192.168.254.17>
2022-06-24 18:41:46 -0400

    test

---
+++ test/src/test/java/calculatorTest.java
@@ -0,0 +1,30 @@
+package calc;
+// Update 2.4
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class calculatorTest {
+	
+
+	@Test
+	void testAdd() {
+		var calc = new calculator();
+		assertEquals(15, calc.add(7,8));
+		}
+
+	@Test
+	void testSubtract() {
+		var calc = new calculator();
+		assertEquals(8, calc.subtract(15,7));
+	}
+
+	@Test
+	void testMultiply() {
+		var calc = new calculator();
+		assertEquals(15, calc.multiply(3,5));
+	}
+	
+	
+
+}
