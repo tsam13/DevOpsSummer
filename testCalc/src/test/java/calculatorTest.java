1cdbcec6773361bb1b8276a4cbfedbcdf72c2b6f
trist <trist@192.168.254.17>
2022-06-27 21:55:03 -0400

    2

---
+++ testCalc/src/test/java/calculatorTest.java
@@ -0,0 +1,27 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+//12
+class calculatorTest {
+
+	@Test
+	void testAdd() {
+		var calc = new calculator();
+		assertEquals(15, calc.add(7,8));
+		}
+
+	@Test
+	void testSubtract() {
+		var calc = new calculator();
+		assertEquals(8, calc.subtract(15,7));
+	}
+
+	@Test
+	void testMultiply() {
+		var calc = new calculator();
+		assertEquals(15, calc.multiply(3,5));
+	}
+	
+	
+
+}
