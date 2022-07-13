58fa5c9b83d3ac98d1ce76284eb61f0bde11dc46
trist <trist@192.168.254.17>
2022-07-13 13:40:26 -0400

    img 1

--- testCalc/src/test/java/calculatorTest.java
+++ testCalc/src/test/java/calculatorTest.java
@@ -46,7 +46,7 @@ class calculatorTest {
 		//assertTrue(driver.getTitle().equals("Welcome to the University of South Florida | Tampa, St. Petersburg, Sarasota-Manatee, FL"));
 		System.out.println("flag 1");
 		Actions action = new Actions(driver);
-		WebElement d=driver.findElement(By.xpath("//h3[@class='subNav_heading']"));  
+		WebElement d=driver.findElement(By.xpath("//img[@alt='New Era. Preeminent Research University.']"));  
 		System.out.println(d);
 		d.click();
 		  //action.moveToElement(d).moveToElement(driver.findElement(By.xpath("//a[@href='/work-at-usf/']"))).click().build().perform();
