f22ffe84215ef5668802edcd07df10bc810ff426
trist <trist@192.168.254.17>
2022-07-13 16:59:06 -0400

    removed window handlers

--- testCalc/src/test/java/calculatorTest.java
+++ testCalc/src/test/java/calculatorTest.java
@@ -48,11 +48,11 @@ class calculatorTest {
 		driver = new ChromeDriver(options);
 		//driver = new ChromeDriver();
 		driver.get("https://www.usf.edu/");
-		String originalWindow = driver.getWindowHandle();
+		//String originalWindow = driver.getWindowHandle();
 		System.out.println(driver.getTitle());
 		assertTrue(driver.getTitle().equals("Welcome to the University of South Florida | Tampa, St. Petersburg, Sarasota-Manatee, FL"));
 			System.out.println("flag 1");
-		Actions action = new Actions(driver);
+		//Actions action = new Actions(driver);
 		WebElement d=driver.findElement(By.xpath("/html/body/header/div[3]/div/nav/ul/li[1]/div[1]/a"));  
 		System.out.println(d);
 		String t = d.getText();
@@ -71,12 +71,12 @@ class calculatorTest {
 	    driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
 	    	System.out.println("flag 4");
 	    //Loop through until we find a new window handle
-	    for (String windowHandle : driver.getWindowHandles()) {
-	        if(!originalWindow.contentEquals(windowHandle)) {
-	            driver.switchTo().window(windowHandle);
-	            break;
-	        }
-	    }
+	  //  for (String windowHandle : driver.getWindowHandles()) {
+	 //       if(!originalWindow.contentEquals(windowHandle)) {
+	 //           driver.switchTo().window(windowHandle);
+	 //           break;
+	 //       }
+	 //   }
 	    title = driver.getTitle();
 	    System.out.println("Page title after link click : " + title);
 	    driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);  
